51dcb51c9a170670ad387e578514a96f67a31e3e
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-07 19:24:39 +0530

    axios credentials

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 import "./index.css";
 import Home from "./components/Home";
 import { GeneralContextProvider } from "./components/GeneralContext";
+
+axios.defaults.withCredentials = true;
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
