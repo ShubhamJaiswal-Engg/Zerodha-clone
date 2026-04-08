ab36bbe6b62fa490d4a1320020110deda1d13229
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-08 15:59:14 +0530

    update frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import ReactDOM from 'react-dom/client';
 import HomePage from './landing_page/home/HomePage';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import axios from "axios";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/products/ProductPage';
@@ -12,6 +13,9 @@ import NavBar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
 import Login from './landing_page/signup/Login'
 import NotFound from './landing_page/NotFound';
+import RedirectIfAuthenticated from "./landing_page/auth/RedirectIfAuthenticated";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,12 +23,12 @@ root.render(
   < NavBar/>
   <Routes>
     <Route path='/' element={<HomePage />} />
-    <Route path='/signup' element={<Signup />} />
+    <Route path='/signup' element={<RedirectIfAuthenticated><Signup /></RedirectIfAuthenticated>} />
     <Route path='/about' element={<AboutPage />} />
     <Route path='/product' element={<ProductPage />} />
     <Route path='/pricing' element={<PricingPage />} />
     <Route path='/support' element={<SupportPage />} />
-    <Route path='/login' element={<Login/>}/>
+    <Route path='/login' element={<RedirectIfAuthenticated><Login /></RedirectIfAuthenticated>}/>
     <Route path="*" element={< NotFound/>} />
   </Routes>
     <Footer/>
