88196a607ffc163876bd19d4feece03e349b8893
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-08 15:30:28 +0530

    updating routes

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,10 +1,11 @@
-const { Signup ,Login, UserData, Me } = require("../controllers/AuthController");
+const { Signup ,Login, UserData, Me, Logout } = require("../controllers/AuthController");
 const { Holdings, Positions, Orders,NewOrder } = require("../controllers/AuthController");
 const router = require("express").Router();
 const { requireAuth } = require("../middleware/requireAuth");
 
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.post("/logout", Logout);
 router.get("/me", requireAuth, Me);
 
 router.get("/allHoldings", requireAuth, Holdings);
