f03d99e01e7f89bcddbdfcb0eff7151e35d23142
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-09 15:45:29 +0530

    update home

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import useAuthStatus from "../auth/useAuthStatus";
 
 function Hero() {
+    const { isCheckingAuth, isAuthenticated } = useAuthStatus();
+
     return ( 
         <div className='Container p-5 mb-5'>
             <div className='row text-center '>
                 <img src='media/images/homeHero.png'  alt='Home Page'/>
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='btn btn-primary p-2 fs-5 mb-5' style={{width:"20%", margin:"0 auto"}} >Sign Up for Free</button>
+                {isCheckingAuth ? null : isAuthenticated ? (
+                    <a
+                        className='btn btn-primary p-2 fs-5 mb-5'
+                        style={{width:"20%", margin:"0 auto"}}
+                        href="http://localhost:3001"
+                    >
+                        Go to Dashboard
+                    </a>
+                ) : (
+                    <Link
+                        className='btn btn-primary p-2 fs-5 mb-5'
+                        style={{width:"20%", margin:"0 auto"}}
+                        to="/signup"
+                    >
+                        Sign Up for Free
+                    </Link>
+                )}
             </div>
             </div>
  );
