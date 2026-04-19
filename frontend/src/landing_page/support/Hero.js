2fc1d2142100ae39de348e46db5013bba5c587c2
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-19 18:18:47 +0530

    Design responsive support

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,28 +2,37 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Hero() {
-    return ( <section className="container-fluid" id='supportwrapper'>
-        <div className="row p-5">
-            <div id='first'><h3>Support Portal</h3>
-            <Link id='link'> Track tickets</Link>
+    return ( 
+      <section className="w-full" id='supportwrapper'>
+        <div className="max-w-6xl mx-auto p-4 md:p-12">
+            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-10 space-y-4 sm:space-y-0" id='first'>
+                <h3 className="text-2xl font-semibold">Support Portal</h3>
+                <Link to="#" id='link' className="font-medium hover:underline">Track tickets</Link>
             </div>
-            <div className="col-6 p-5">
-                <h3 className="fs-4 mb-4">Search for an answer or browse help topics to create a ticket</h3>
-                <input className='mb-4' id='input' style={{outline:"none"}} placeholder='Eg: how do i activate F&O, why is my order getting rejected ...' /><br/>
-                <Link id='link-down' > Track account opening</Link>
-                <Link id='link-down'>  Track segment activation</Link>
-                <Link id='link-down' >  Intraday margins</Link>
-                <Link id='link-down'> Kite user manual</Link>
-            </div>
-            <div className="col-6 p-5">
-                <h3>Featured</h3>
-                <ol>
-                    <li id='ol'><Link id='li'>MCX Option contracts expiry - April 2025</Link></li>
-                    <li><Link id='li'>MCX Base metals contract expiry - April 2025</Link></li>
-                </ol>
+            
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-16">
+                <div className="space-y-6">
+                    <h3 className="text-xl md:text-2xl leading-relaxed">Search for an answer or browse help topics to create a ticket</h3>
+                    <input className='w-full px-4 py-3 rounded border-0 shadow-sm focus:ring-2 focus:ring-blue-300 outline-none text-gray-800' id='input' placeholder='Eg: how do i activate F&O, why is my order getting rejected ...' />
+                    <div className="flex flex-wrap gap-x-6 gap-y-3 mt-4">
+                        <Link to="#" id='link-down' className="hover:underline">Track account opening</Link>
+                        <Link to="#" id='link-down' className="hover:underline">Track segment activation</Link>
+                        <Link to="#" id='link-down' className="hover:underline">Intraday margins</Link>
+                        <Link to="#" id='link-down' className="hover:underline">Kite user manual</Link>
+                    </div>
+                </div>
+                
+                <div className="space-y-6 md:pl-10">
+                    <h3 className="text-xl md:text-2xl font-semibold">Featured</h3>
+                    <ol className="list-decimal ml-6 space-y-4">
+                        <li id='ol'><Link to="#" id='li' className="hover:underline">MCX Option contracts expiry - April 2025</Link></li>
+                        <li><Link to="#" id='li' className="hover:underline">MCX Base metals contract expiry - April 2025</Link></li>
+                    </ol>
+                </div>
             </div>
         </div>
-    </section> );
+      </section> 
+    );
 }
 
 export default Hero;
