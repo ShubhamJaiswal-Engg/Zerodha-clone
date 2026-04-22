ffcf1de5f9dfb0dd073288742f4e6f79db9e0931
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-22 23:25:05 +0530

    update product section

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function RightSection({imageURL,tryDemo,googlePlay,appStore,learnMore,productDescription,ProductName}) {
-    return (<div className="container">
-        <div className="row p-5">
-            <div className="col-6 p-5">
-                <h1 style={{marginTop:"110px"}} className="mx-5">{ProductName}</h1>
-                <p className="mx-5">{productDescription}</p>
-                <div className='mx-5'>
-                    <Link to={"*"} style={{marginRight:"80px" , textDecoration:"none"}} >{learnMore} <i class="fa-solid fa-arrow-right"></i></Link>
+    return (<div className="container mx-auto p-4 md:p-12 mb-4">
+        <div className="flex flex-col-reverse md:flex-row items-center gap-8 md:gap-12 max-w-6xl mx-auto">
+            <div className="w-full md:w-1/2 p-4 md:p-10 space-y-6">
+                <h1 className="text-3xl md:text-4xl font-semibold text-gray-800">{ProductName}</h1>
+                <p className="text-gray-600 text-base md:text-lg leading-relaxed">{productDescription}</p>
+                <div>
+                    <Link to={"#"} className="text-blue-500 hover:text-blue-700 font-medium hover:underline" >{learnMore} <i className="fa-solid fa-arrow-right"></i></Link>
                 </div>
             
             </div>
-            <div className="col-6">
-                <img alt='hero' src={imageURL} />
+            <div className="w-full md:w-1/2 flex justify-center px-4">
+                <img alt='hero' className="w-4/5 md:w-full" src={imageURL} />
             </div>
         </div>
     </div> );
