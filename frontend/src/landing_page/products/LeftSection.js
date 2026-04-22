02b16de4ef5b3b83fb82c33e8e549e3eea096665
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-22 23:25:36 +0530

    update product section

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function LeftSection({imageURL,tryDemo,googlePlay,appStore,learnMore,productDescription,ProductName}) {
-    return ( <div className="container">
-        <div className="row p-5 mt-3">
-            <div className="col-6">
-                <img alt='hero' src={imageURL} />
+    return ( <div className="container mx-auto p-4 md:p-12 mb-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 items-center max-w-6xl mx-auto">
+            <div className="flex justify-center p-4">
+                <img alt='hero' src={imageURL} className="w-4/5 md:w-full" />
             </div>
-            <div className="col-6 p-5">
-                <h1 className="mx-5">{ProductName}</h1>
-                <p className="mx-5">{productDescription}</p>
-                <div className='mx-5'>
-                    <Link to={"*"} style={{marginRight:"80px" , textDecoration:"none" }} >{tryDemo} <i class="fa-solid fa-arrow-right"></i></Link>
-                    <Link to={"*"} style={{marginRight:"80px" , textDecoration:"none"}} >{learnMore} <i class="fa-solid fa-arrow-right"></i></Link>
+            <div className="p-4 md:p-10 space-y-6">
+                <h1 className="text-3xl md:text-4xl font-semibold text-gray-800">{ProductName}</h1>
+                <p className="text-gray-600 text-base md:text-lg leading-relaxed">{productDescription}</p>
+                <div className='flex gap-8'>
+                    <Link to={"#"} className="text-blue-500 hover:text-blue-700 font-medium hover:underline" >{tryDemo} <i className="fa-solid fa-arrow-right"></i></Link>
+                    <Link to={"#"} className="text-blue-500 hover:text-blue-700 font-medium hover:underline" >{learnMore} <i className="fa-solid fa-arrow-right"></i></Link>
                 </div>
-                <div className="mx-5 mt-4">
-                    <img alt='logo' style={{marginRight:"20px"}} src={googlePlay} />
-                    <img alt='logo' src={appStore} />
+                <div className="flex gap-4 mt-4">
+                    <img alt='logo' className="h-10 cursor-pointer" src={googlePlay} />
+                    <img alt='logo' className="h-10 cursor-pointer" src={appStore} />
 
                 </div>
             </div>
