bb58ccaa9e5043a74ff39304763cdabc3b6a5503
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-24 23:11:25 +0530

    conditional button

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import useAuthStatus from "./auth/useAuthStatus.js";
+
+
 function OpenAccount() {
-    return (  <div className='Container p-5 mb-5'>
-        <div className='row text-center '>
-            <h1 className='mt-5 mb-3'>Open a Zerodha account</h1>
-            <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-            <button className='btn btn-primary p-2 fs-5 mt-3' style={{width:"20%", margin:"0 auto"}} >Sign up for Free</button>
+    const { isCheckingAuth, isAuthenticated } = useAuthStatus();
+    return (  <div className='container mx-auto p-4 mb-12'>
+        <div className='flex flex-col items-center text-center'>
+            <h1 className='mt-12 mb-6 text-4xl font-semibold text-gray-800'>Open a StockX account</h1>
+            <p className="text-xl text-gray-600 mb-8">Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+             {isCheckingAuth ? null : isAuthenticated ? (
+                    <a
+                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-lg mb-12'
+                        href="http://localhost:3001"
+                    >
+                        Go to Dashboard
+                    </a>
+                ) : (
+                    <Link
+                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded text-lg mb-12'
+                        to="/signup"
+                    >
+                        Sign Up for Free
+                    </Link>
+                )}
+                
         </div>
         </div> );
 }
