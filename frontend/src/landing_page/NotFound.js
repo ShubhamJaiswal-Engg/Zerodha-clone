5cf8d1594fb7bddf4ca6f6b3c0785e92a83e78e5
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-24 23:13:18 +0530

    update not found

--- frontend/src/landing_page/NotFound.js
+++ frontend/src/landing_page/NotFound.js
@@ -1,11 +1,9 @@
 import React from 'react'
 function NotFound() {
-    return (  <div className='Container p-5 mb-5'>
-        <div className='row'>
-            <h1 className='mt-5 mb-3'>404 Not Found</h1>
-            <p>sorry, the page you are looking for does not exist</p>
-        </div>
-        </div> );
+    return (  <div className='container mx-auto p-12 mb-12 flex flex-col items-center text-center'>
+            <h1 className='mt-12 mb-6 text-4xl font-semibold text-gray-800'>404! Not Found</h1>
+            <p className="text-xl text-gray-600">sorry, the page you are looking for does not exist</p>
+        </div>);
 }
 
 export default NotFound;
