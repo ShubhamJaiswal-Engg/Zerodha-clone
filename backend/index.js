7fe2cdb9f87b8673589528b04e2ee7f568b494c2
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-26 12:26:35 +0530

    update index

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
 
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001" ];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 app.use(cors({
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin)){
@@ -21,7 +21,6 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
-// app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.json());
