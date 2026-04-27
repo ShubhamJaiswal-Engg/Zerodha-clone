c9928b637e2f30961359ea226067f6ba71443347
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-04-27 19:36:42 +0530

    change logo size

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import Menu from "./Menu";
 
+const getPageTitle = (pathname) => {
+  switch (pathname) {
+    case "/":
+      return "Dashboard";
+    case "/orders":
+      return "Orders";
+    case "/holdings":
+      return "Holdings";
+    case "/positions":
+      return "Positions";
+    case "/funds":
+      return "Funds";
+    case "/apps":
+      return "Apps";
+    case "/marketwatch":
+      return "Marketwatch";
+    default:
+      return "Dashboard";
+  }
+};
+
 const TopBar = () => {
+  const { pathname } = useLocation();
+  const pageTitle = getPageTitle(pathname);
+
   return (
     <div className="topbar-container">
-      <div className="indices-container">
+      <div className="indices-container" aria-hidden="true">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
           <p className="index-points">{100.2} </p>
@@ -18,7 +43,7 @@ const TopBar = () => {
         </div>
       </div>
 
-      <Menu />
+      <Menu pageTitle={pageTitle} />
     </div>
   );
 };
