659f0f5bed1672a3919741099ff916f021ab78c3
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-07 23:40:19 +0530

    update hero section

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -6,23 +6,21 @@ function Hero() {
     const { isCheckingAuth, isAuthenticated } = useAuthStatus();
 
     return ( 
-        <div className='Container p-5 mb-5'>
-            <div className='row text-center '>
-                <img src='media/images/homeHero.png'  alt='Home Page'/>
-                <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+        <div className='container mx-auto p-4 pb-0'>
+            <div className='flex flex-col items-center text-center'>
+                <img src='media/images/homeHero.png' alt='Home Page' className="mb-8" />
+                <h1 className='mt-12 text-4xl font-semibold text-gray-800 mb-2'>Invest in everything</h1>
+                <p className="text-xl text-gray-600 mb-8">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
                 {isCheckingAuth ? null : isAuthenticated ? (
                     <a
-                        className='btn btn-primary p-2 fs-5 mb-5'
-                        style={{width:"20%", margin:"0 auto"}}
+                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-lg mb-12'
                         href="http://localhost:3001"
                     >
                         Go to Dashboard
                     </a>
                 ) : (
                     <Link
-                        className='btn btn-primary p-2 fs-5 mb-5'
-                        style={{width:"20%", margin:"0 auto"}}
+                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded text-lg mb-12'
                         to="/signup"
                     >
                         Sign Up for Free
