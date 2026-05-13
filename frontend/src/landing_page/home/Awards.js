2f1c3940513d9f2c62aaa988318193373d73340c
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-13 23:55:22 +0530

    change text in  award section

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -3,20 +3,20 @@ import React from 'react'
 function Awards() {
     return ( 
        <div>
-        <div className='Container mt-5'>
-            <div className='row'>
-                <div className='col-6 p-5'>
-                    <img src='media/images/largestBroker.svg' alt='Brokertag' />
+        <div className='container mx-auto mt-1'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
+                <div className='p-4'>
+                    <img src='media/images/largestBroker.svg' className='w-full' alt='Brokertag' />
                 </div>
-                <div className='col-6 p-5 mt-5'>
-                    <h1>Largest Broker Exchange in India</h1>
-                    <p className="mb-5">
-            2+ million Zerodha clients contribute to over 15% of all retail
+                <div className='p-4'>
+                    <h1 className="text-3xl font-semibold text-gray-800 mb-6">Largest Broker Exchange in India</h1>
+                    <p className="mb-12 text-gray-600">
+                      2+ million StockX clients contribute to over 15% of all retail
             order volumes in India daily by trading and investing in:
           </p>
-          <div className="row">
-            <div className="col-6">
-              <ul>
+          <div className="flex justify-between mb-8">
+            <div className="w-1/2">
+              <ul className="list-disc ml-6 space-y-4 text-gray-600">
                 <li>
                   <p>Futures and Options</p>
                 </li>
@@ -28,8 +28,8 @@ function Awards() {
                 </li>
               </ul>
               </div>
-              <div className="col-6">
-              <ul>
+              <div className="w-1/2">
+              <ul className="list-disc ml-6 space-y-4 text-gray-600">
                 <li>
                   <p>Stocks & IPOs</p>
                 </li>
@@ -41,8 +41,8 @@ function Awards() {
                 </li>
               </ul>
               </div>
-              <img src='media/images/pressLogos.png' alt='preetag' style={{ width:"80%"}} />
-                </div>
+              </div>
+              <img  src='media/images/pressLogos.png' alt='preetag' className="ml-3.5 w-4/5 mt-4" />
                 </div>
             </div>
         </div>
