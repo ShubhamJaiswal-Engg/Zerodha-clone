196e9ba6a970dde35374720f5091878aa2e2354a
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-16 19:44:53 +0530

    ..

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
  function Pricing() {
     return (
-      <div className="container p-2">
-         <div className="row p-2">
-            <div className="col-6">
-               <h1 className="fs-2 mb-3"> Unbeatable pricing</h1>
-               <p className='text-muted'>We pioneered the concept of discount broking and price <br></br> in India. Flat fees and no hidden charges.</p>
-               <a href='*' style={{textDecoration:"none"}}> See pricing <i class="fa-solid fa-arrow-right"></i></a>
+      <div className="container mx-auto p-4">
+         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 items-center">
+            <div>
+               <h1 className="text-3xl font-semibold text-gray-800 mb-12"> Unbeatable pricing</h1>
+               <p className='text-gray-500 mb-4'>We pioneered the concept of discount broking and price <br></br> in India. Flat fees and no hidden charges.</p>
+               <Link className="text-blue-500 hover:text-blue-700 font-medium hover:underline"> See pricing <i className="fa-solid fa-arrow-right"></i></Link>
             </div>
-            <div className="col-6 mb-5">
-               <div className="row">
-               <div className='col-4'><img src='media/images/pricing0.svg'  width={"65%"} alt='rs'/> 
-               <p style={{fontSize:"10px"}}>Free account <br></br>
+            <div className="mb-12">
+               <div className="flex justify-between items-center text-center">
+               <div className='w-1/3 flex flex-col items-center'><img src='media/images/pricing0.svg' className="w-2/3 mb-2" alt='rs'/> 
+               <p className="text-xs text-gray-500">Free account <br></br>
                opening</p></div>
-               <div className='col-4'><img src='media/images/pricing0.svg'  width={"65%"} alt='rs'/>
-               <p style={{fontSize:"10px"}}>Free equity delivery <br></br>
+               <div className='w-1/3 flex flex-col items-center'><img src='media/images/pricing0.svg' className="w-2/3 mb-2" alt='rs'/>
+               <p className="text-xs text-gray-500">Free equity delivery <br></br>
                and direct mutual funds</p>
                </div>
-               <div className='col-4'><img src='media/images/intradayTrades.svg' width={"65%"} alt='rs'/>
-               <p style={{fontSize:"10px"}}>Intraday and
+               <div className='w-1/3 flex flex-col items-center'><img src='media/images/intradayTrades.svg' className="w-2/3 mb-2" alt='rs'/>
+               <p className="text-xs text-gray-500">Intraday and
              <br></br>
              F&O</p></div>
             </div>
