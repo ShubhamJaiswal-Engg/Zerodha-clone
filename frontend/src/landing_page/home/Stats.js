b77566efb3b8c233d250ca6c302bceecf38be6b5
Shubham Jaiswal <shubhamjai8662@gmail.com>
2026-05-16 19:48:44 +0530

    designing responsive home component

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,34 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Stats() {
     return ( 
-        <div className="Container p-2">
-        <div className="row p-5">
-            <div className="col-6 p-5">
-                <h1 className='fs-2 mb-5'>Trust with confidence</h1>
-                <h2 className='fs-4'>Customer-first always</h2>
-                <p className='text-muted mb-4'>That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+        <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            <div className="p-4">
+                <h1 className='text-3xl font-semibold mb-12 text-gray-800'>Trust with confidence</h1>
+                <h2 className='text-xl font-medium text-gray-800 mb-2'>Customer-first always</h2>
+                <p className='text-gray-500 mb-8'>That's why 1.5+ crore customers trust StockX with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
                  
-                <h2 className='fs-4'>No spam or gimmicks</h2>
-                <p className='text-muted mb-4'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+                <h2 className='text-xl font-medium text-gray-800 mb-2'>No spam or gimmicks</h2>
+                <p className='text-gray-500 mb-8'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
                 
-                <h2 className='fs-4'>The Zerodha universe</h2>
-                <p className='text-muted mb-4'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+                <h2 className='text-xl font-medium text-gray-800 mb-2'>The StockX universe</h2>
+                <p className='text-gray-500 mb-8'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
 
 
-                <h2 className='fs-4'>Do better with money</h2>
-                <p className='text-muted mb-4'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+                <h2 className='text-xl font-medium text-gray-800 mb-2'>Do better with money</h2>
+                <p className='text-gray-500 mb-8'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
 
             </div>
-            <div className="col-6 text-center">
-                <img src='media/images/ecosystem.png'  alt='ecosystem' style={{ width: "90%"}}/>
-                <div>
-                    <a href='*' className='mx-5' style={{textDecoration:"none"}}> Explore our products <i class="fa-solid fa-arrow-right"></i></a>
-                    <a href='*'  style={{textDecoration:"none"}}> Try kite demo <i class="fa-solid fa-arrow-right"></i></a>
+            <div className="text-center flex flex-col items-center p-4">
+                <img src='media/images/ecosystem.png' alt='ecosystem' className="w-11/12 mb-6"/>
+                <div className="flex gap-8">
+                    <Link  className='text-blue-500 hover:text-blue-700 font-medium hover:underline'> Explore our products <i className="fa-solid fa-arrow-right"></i></Link>
+                    <Link  className='text-blue-500 hover:text-blue-700 font-medium hover:underline'> Try kite demo <i className="fa-solid fa-arrow-right"></i></Link>
                 </div>
             </div>
-            <div className="text-center mt-5">
-            <img src='media/images/pressLogos.png' alt='preetag' style={{ width:"55%"}} />
+            <div className="col-span-1 md:col-span-2 flex justify-center mt-12">
+              <img src='media/images/pressLogos.png' alt='preetag' className="w-3/5" />
             </div>
         </div>
         </div>
